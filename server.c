370678437315df08f9168540be14a024e6554f02
Saif MacBook <saif@Saifs-MacBook-Air.local>
2024-11-05 00:52:58 +0400

    version 1
    A command not done, R command done but not tested

---
+++ server.c
@@ -0,0 +1,51 @@
+#include "server_helper.h"
+
+void usage()
+{
+    printf("server.exe -i or\n server.exe <portnumber>\n");
+}
+
+
+// public static main(string[] args)
+int main(int argc, char** argv)
+{
+
+    CmdArg  cmd;
+    if(!process_args(argc, argv, &cmd)){
+        usage();
+        return -1;   
+    };
+    if (cmd.is_interactive) //if -i was provided
+    {
+        run_interactive(&cmd); //we have the address of the cmd here so it doesnt make a copy
+    }
+    else
+    {
+        run_listen(&cmd);
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO1 true/false ()  -DONE
+//TODO2 write usage() function
+//TODO3 call usage() if process_args failes - DONE
+//TODO4 write process_cmd function   -DONE
+//TODO5 finsihs the run_interactive functions inside    R Done,
+//TODO5.1 implement the isValidRule function to help run_add_cmd (A)
+//TODO5.1.1 make the isValidPort and isValidIP function to help isValidRule 
+
+
---
+++ server_helper.h
@@ -0,0 +1,360 @@
+#include<stdbool.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+#define MAX_FW_CMD 255
+#define MAX_FW_RULE 255
+#define MAX_FW_QUERY 255
+
+typedef struct CmdArg
+{
+    bool is_interactive;
+    int port;
+} CmdArg;
+
+typedef struct FwRequest
+{
+    char RawCmd[MAX_FW_CMD];
+    char Cmd;
+    struct FwRequest* pNext;
+} FwRequest;
+
+typedef struct FwRule
+{
+    //ipAdress 1, 2
+    //port 1, 2
+    char ip1[15];
+    char ip2[15];
+    int port1;
+    int port2;
+
+    struct FwRule* pNext;
+} FwRule;
+
+typedef struct Query
+{
+    char rawQuery[MAX_FW_QUERY];
+    char query;
+    struct Query* pNext;
+} Query;
+
+
+bool is_digit(char c)
+{
+    if (c >= '0' && c <= '9'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+//I will make a power function, so 2^3 = 8
+int powi(int b, int e)
+{
+    int result = 1;
+    for (int i = 0; i < e; i++)
+    {
+        result *= b;
+    }
+    return result;
+}
+
+bool is_integer(char* pch, int* num)
+{
+   *num = 0;
+   for (int i = 0; i < strlen(pch); i++)
+   {
+       if (is_digit(pch[i]))
+       {
+           *num += powi(10, i)*(pch[i] - 48);
+       }
+       else{
+        return false;
+       }
+   }
+   return true;
+}
+
+//adds a request to the end of the linked list
+void add_to_Req_list(FwRequest* fwReq, FwRequest* fwReqHead)
+{
+    if (fwReqHead == NULL)
+    {
+        fwReqHead = fwReq;
+        return;
+    }
+    FwRequest* cur = fwReqHead;
+    while (cur->pNext != NULL)
+    {
+        cur = cur->pNext;
+    }
+    cur->pNext = fwReq;
+}
+
+//adds a rule to the end of the linked list
+void add_to_Rule_list(FwRule* fwRule, FwRule* fwRuleHead)
+{
+    
+    if (fwRuleHead == NULL)
+    {
+        fwRuleHead = fwRule;
+        return;
+    }
+    FwRequest* cur = fwRuleHead;
+    while (cur->pNext != NULL)
+    {
+        cur = cur->pNext;
+    }
+    cur->pNext = fwRule;
+    */
+   printf("add_to_Rule_list\n");
+   return;
+}
+
+
+bool isValidIP(FwRule* fwRule){
+    char firstIP[15];
+    char secIP[15];
+    bool smaller = false;
+    bool correctSyntax = false;
+    bool correctValues = false;
+
+    //we do this to avoid changing the original values
+    strcpy(firstIP, fwRule->ip1);
+    strcpy(secIP, fwRule->ip2);
+
+    int p1split[4];
+    int p2split[4];
+
+    //here we use scanf to split them into 4 parts so we can compare size
+    if (scanf(firstIP, "%d.%d.%d.%d", &p1split[0], &p1split[1], &p1split[2], &p1split[3]) ==4 &
+    scanf(secIP, "%d.%d.%d.%d", &p2split[0], &p2split[1], &p2split[2], &p2split[3]) == 4)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (p1split[i] < 0 || p1split[i] > 255 || p2split[i] < 0 || p2split[i] > 255)
+            {
+                correctValues = false;
+                break;
+            }
+            else{
+                correctValues = true;
+            }
+        }
+
+        correctSyntax = true;
+    }
+
+
+    if(correctSyntax && correctValues)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (p1split[i] < p2split[i])
+            {
+                smaller = true;
+                break;
+            }
+            else if (p1split[i] > p2split[i])
+            {
+                break;
+            }
+        }
+    }
+    return smaller;
+
+
+
+}
+
+bool isValidPort(FwRule* fwRule){
+    bool correctValues = false;
+    bool smaller = false;
+
+    if (fwRule->port1 >= 0 && fwRule->port1 <= 65535 && fwRule->port2 >= 0 && fwRule->port2 <= 65535)
+    {
+        correctValues = true;
+    }
+
+    if (correctValues)
+    {
+        if (fwRule->port1 < fwRule->port2)
+        {
+            smaller = true;
+        }
+    }
+    return smaller;
+}
+
+
+//if the rule is valid then return true
+bool isValidRule(FwRule* fwRule)
+{
+    if (isValidIP(fwRule) && isValidPort(fwRule))
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+bool process_args(int argc, char** argv, CmdArg* pcmd)
+{
+    pcmd->is_interactive = false; //default value
+    pcmd->port = 0; //default value
+    bool bSucess = false;
+
+    if (argc != 2) {
+        return false;}
+    bSucess = false;
+    
+
+    if (strcmp(argv[1], "-i") == 0){
+        pcmd->is_interactive = true;
+        bSucess = true;
+        }
+    else{
+        if (is_integer(argv[1], &pcmd->port)){
+            pcmd->is_interactive = false;
+            bSucess = true;
+        }
+    }
+    return bSucess;
+}
+
+FwRule* process_req_cmd(char* buffer)
+{
+    FwRule* fwRule = (FwRule*)malloc(sizeof(FwRule));
+    if (fwRule == NULL)
+    {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    char* pch;
+
+
+
+
+
+
+    return fwRule;
+}
+
+
+void run_add_cmd(FwRequest* fwReq, FwRule* fwRuleHead)
+{
+    char buffer [255];
+    printf("running add\n");
+    
+    printf("enter command\n");
+    fgets(buffer, 255, stdin);
+    FwRule* fwRule = process_req_cmd(buffer);
+
+
+    FwRule* fwRule = (FwRule*)malloc(sizeof(FwRule));
+    if (isValidRule(fwRule)){
+        add_to_rule_list(fwRule, fwRuleHead);
+        printf("Rule added\n");
+    }
+    else{
+        printf("Invalid Rule\n");
+    }
+    
+}
+
+void run_del_cmd(FwRequest* fwReq, FwRule* fwRuleHead)
+{
+    printf("running del\n");
+}
+
+void run_list_cmd(FwRequest* fwReq, FwRule* fwRuleHead)
+{
+    printf("running list\n");
+}
+
+void run_list_requests_cmd(FwRequest* fwReq, FwRequest* fwReqHead)
+{
+    printf("running list requests\n");
+    FwRequest* cur = fwReqHead;
+
+    if (cur == NULL)
+    {
+        printf("No requests\n");
+        return;
+    }
+    while (cur -> pNext != NULL)
+    {
+        printf("%s\n", cur->RawCmd);
+        cur = cur->pNext;
+    }
+}
+
+void run_check_cmd(FwRequest* fwReq, FwRequest* fwReqHead)
+{
+    printf("running check\n");
+}
+
+
+FwRequest* process_cmd(char* buffer)
+{
+    FwRequest* fwReq = (FwRequest*)malloc(sizeof(FwRequest));
+    strcpy(fwReq->RawCmd, buffer);
+    fwReq->Cmd = buffer[0];
+    return fwReq;
+}
+
+void run_interactive(CmdArg* pcmd)
+{
+    printf("running interactive\n");
+    char buffer[255];
+    FwRule * fwRuleHead = NULL;
+    FwRequest* fwReqHead = NULL;
+
+    while (true)
+    {
+
+        printf("Enter a command: ");
+        fgets(buffer, 255, stdin);
+        FwRequest* fwReq = process_cmd(buffer);
+        add_to_Req_list(fwReq, fwReqHead);
+        switch (fwReq->Cmd)
+        {
+        case 'A':
+            run_add_cmd(fwReq, fwRuleHead);
+            break;
+        case 'D':
+            run_del_cmd(fwReq, fwRuleHead);
+            break;
+        case 'L':
+            run_list_cmd(fwReq, fwRuleHead);
+            break;
+        case 'R':
+            run_list_requests_cmd(fwReq, fwReqHead);
+            break;
+        case 'C':
+            run_check_cmd(fwReq, fwReqHead);
+            break;
+        case 'Q':
+            //DO NOT FORGET TO FREE THE MEMORY
+            printf("Exiting\n");
+            return;
+
+        default:
+            printf("Illegal Request\n");
+            break;
+        }
+    }
+}
+
+void run_listen(CmdArg* pcmd)
+{
+    printf("running listen on port %d\n", pcmd->port);
+}
+
+
+
